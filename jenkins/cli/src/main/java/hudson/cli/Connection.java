0b6b1221567dd5f031239077384621361b890959
Hanley Spencer <spencerhanley1@gmail.com>
2020-10-30 21:17:54 -0400

    q5

--- jenkins/cli/src/main/java/hudson/cli/Connection.java
+++ jenkins/cli/src/main/java/hudson/cli/Connection.java
@@ -153,9 +153,7 @@ public class Connection {
             AlgorithmParameterGenerator paramGen = AlgorithmParameterGenerator.getInstance("DH");
             paramGen.init(keySize);
 
-            KeyPairGenerator dh = KeyPairGenerator.getInstance("DH");
-            dh.initialize(paramGen.generateParameters().getParameterSpec(DHParameterSpec.class));
-            keyPair = dh.generateKeyPair();
+            keyPair = generateKeyPairWithSpec(paramGen.generateParameters().getParameterSpec(DHParameterSpec.class));
 
             // send a half and get a half
             writeKey(keyPair.getPublic());
@@ -163,9 +161,7 @@ public class Connection {
         } else {
             otherHalf = KeyFactory.getInstance("DH").generatePublic(readKey());
 
-            KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("DH");
-            keyPairGen.initialize(((DHPublicKey) otherHalf).getParams());
-            keyPair = keyPairGen.generateKeyPair();
+            keyPair = generateKeyPairWithSpec(((DHPublicKey) otherHalf).getParams());
 
             // send a half and get a half
             writeKey(keyPair.getPublic());
@@ -176,6 +172,12 @@ public class Connection {
         ka.doPhase(otherHalf, true);
 
         return ka;
+    }
+
+    public KeyPair generateKeyPairWithSpec(DHParameterSpec d) throws IOException, GeneralSecurityException {
+        KeyPairGenerator dh = KeyPairGenerator.getInstance("DH");
+        dh.initialize(d);
+        return dh.generateKeyPair();
     }
 
     /**
--- jenkins/core/src/test/java/jenkins/model/idFromFileNameTest.java
+++ jenkins/core/src/test/java/jenkins/model/idFromFileNameTest.java
@@ -8,16 +8,17 @@ import org.junit.rules.TemporaryFolder;
 import org.jvnet.hudson.test.Issue;
 
 public class idFromFileNameTest {
+	public IdStrategy.CaseSensitive x  = new IdStrategy.CaseSensitive();
 
     @Test
     public void testB()
     {
-    	assertEquals("b", IdStrategy.idFromFilename("$0062"));
+    	assertEquals("b", x.idFromFilename("$0062"));
     }
 
     @Test
     public void testA()
     {
-    	assertEquals("a", IdStrategy.idFromFilename("$0061"));
+    	assertEquals("a", x.idFromFilename("$0061"));
     }
 }
